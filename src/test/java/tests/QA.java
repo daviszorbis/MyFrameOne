34775be13388d76897dcd6654be2e647dc3c76d1
dsharma <dsharma@COG-VDI-QA.devlabs.com>
2022-06-08 17:38:48 +0530

    This is first commmit of Frame1 project.

---
+++ src/test/java/tests/QA.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class QA {
+	
+	@Test
+    public void exampleOfTestNgMaven() {
+        System.out.println("This is TestNG-Maven Example");
+    }
+
+}
